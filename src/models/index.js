a51677764b4ee2ed108d34791e4e1d465cf33272
ejjem <tigonlee0627@gmail.com>
2024-08-05 03:05:10 +0900

    joinquipuModels 수정

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const fs = require('fs');
 const path = require('path');
 const env = process.env.NODE_ENV || 'test';
-const config = require(__dirname + '/../config/config.json')['test'];
+const config = require(__dirname + '/../config/config.json')[env];
 
 const db = {}
 const sequelize = new Sequelize( config.database, config.username, config.password, config);
