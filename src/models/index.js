2f77e8a7dc2f2ab2802415bde8d455b370d46dc5
이제민 <ejjem@ijemin-ui-MacBookAir.local>
2024-08-05 15:12:36 +0900

    update

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 const fs = require('fs');
 const path = require('path');
-const env = process.env.NODE_ENV || 'test';
-const config = require(__dirname + '/../config/config.json')['test'];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 
 const db = {}
 const sequelize = new Sequelize( config.database, config.username, config.password, config);
