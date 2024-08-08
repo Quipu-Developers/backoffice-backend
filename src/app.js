4cf6e404c73c0a41823bd23da5e255e0c9e159e0
ejjem <tigonlee0627@gmail.com>
2024-08-08 20:35:09 +0900

    cors 수정

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 
 //cors
 app.options((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
